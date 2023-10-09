af9bcf74a54f7bafb1e095938efa51339eab59a1
louisecastrof <louise.castro@grupoboticario.com.br>
2023-10-09 10:06:56 -0300

    :sparkles: First Component

--- React/1_INTRO/hello-world/src/App.js
+++ React/1_INTRO/hello-world/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './components/FirstComponent';
 
 function App() {
   return (
     <div className="App">
       <h1> Hello World React!</h1>
+      <FirstComponent />
     </div>
   );
 }
---
+++ React/1_INTRO/hello-world/src/components/FirstComponent.js
@@ -0,0 +1,12 @@
+function FirstComponent() {
+// Comentários
+    
+    return (
+        <div className="firstcomponent">
+            {/* Comentário no JSX */}
+            <p>Primeiro Component</p>
+        </div>
+    )
+}
+
+export default FirstComponent
