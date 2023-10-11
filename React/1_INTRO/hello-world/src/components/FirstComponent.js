8aefbeb27b59e0ca3f59fb8128ed786e7ff3fca8
louisecastrof <louise.castro@grupoboticario.com.br>
2023-10-11 10:30:56 -0300

    :sparkles: eventos de clique

--- React/1_INTRO/hello-world/src/components/FirstComponent.js
+++ React/1_INTRO/hello-world/src/components/FirstComponent.js
@@ -1,10 +1,18 @@
+import AnotherComponent from "./AnotherComponent"
+
 function FirstComponent() {
 // Comentários
+const name = 'Louise'
     
     return (
         <div className="firstcomponent">
             {/* Comentário no JSX */}
             <p>Primeiro Component</p>
+            {/* Rodado pelo JavaScript */}
+            {2 + 2}
+            {/* Função definida fora do JSX */}
+            <p>Nome: {name}</p>
+            <AnotherComponent />
         </div>
     )
 }
