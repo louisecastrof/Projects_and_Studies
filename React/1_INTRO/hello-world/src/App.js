233b7e55466fa0040a9dcfcb9863fe5d3e0977c3
louisecastrof <louise.castro@grupoboticario.com.br>
2023-10-26 07:28:00 -0300

    :sparkles: hook idade

--- React/1_INTRO/hello-world/src/App.js
+++ React/1_INTRO/hello-world/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import FirstComponent from './components/FirstComponent';
 import AnotherComponent from './components/AnotherComponent';
 import Images from './components/Images';
+import Hooks from './components/Hooks';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <FirstComponent />
       < AnotherComponent />
       <Images/ >
+      <Hooks />
     </div>
   );
 }
