dae232ace9b6f117e42ea482129005ca89f4c334
louisecastrof <louise.castro@grupoboticario.com.br>
2023-10-26 07:18:24 -0300

    :sparkles: add image

--- React/1_INTRO/hello-world/src/App.js
+++ React/1_INTRO/hello-world/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import FirstComponent from './components/FirstComponent';
 import AnotherComponent from './components/AnotherComponent';
+import Images from './components/Images';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1> Hello World React!</h1>
       <FirstComponent />
       < AnotherComponent />
+      <Images/ >
     </div>
   );
 }
---
+++ React/1_INTRO/hello-world/src/components/Images.js
@@ -0,0 +1,11 @@
+import lisbon from '../assets/f5.jpg'
+
+const Images = () => {
+    return (
+        <div>
+            <img src="/f2.jpeg" a />
+            <img src={lisbon} />
+        </div>
+    )
+}
+export default Images
